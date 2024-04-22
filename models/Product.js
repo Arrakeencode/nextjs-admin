ccfd94b5f6cf5f82bc09e54f70fd901617b2964b
Arrakeencode <bryanproinfo@gmail.com>
2024-04-22 22:17:58 +0200

    Update product

--- models/Product.js
+++ models/Product.js
@@ -4,8 +4,8 @@ const ProductSchema = new Schema({
     title: {type: String, required:true},
     description: {type: String, required:true},
     price: {type: Number, required: true},
-    category: {type: String, required: true},
-    images: [{type: String, required: true}]
+    category: {type: String},
+    images: [{type: String}]
 });
 
 export const Product = models.Product || model('Product', ProductSchema);
